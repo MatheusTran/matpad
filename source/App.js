16071baaf3bd682aa183424fc9e40f97f7fb6598
MatheusTran <matheustran@gmail.com>
2026-02-21 18:44:55 +0100

    forgot to remove some debug stuff

--- source/App.js
+++ source/App.js
@@ -34,29 +34,29 @@ export function App() {
 	// it current recomputes everything. Might find a way to compute only the lines that are changed.
 	// Could look into the way Julia (the programming language) does things
 	const renderOutput = () => {
-		// let scope = {} // scope is where all the variables are stored basically.
+		let scope = {} // scope is where all the variables are stored basically.
 
-		// // evaluate every line, and append it to the output
-		// const outputs = text.map((expr) => {
-		// 	if (!expr) {
-		// 		return ""
-		// 	}
-		// 	let msg = ""
-		// 	try {
-		// 		msg = String(evaluate(expr, scope))
-		// 	} catch (err) {
-		// 		msg = String(err.message)
-		// 	}
-		// 	// if line too long, truncate it
-		// 	msg = msg.replace("\n", " ")
-		// 	if (msg.length > columns / 2) {
-		// 		msg = msg.slice(0, Math.floor(columns / 2) - 7) + "..."
-		// 		// magic number 7. comes from 2 characters from the border, 2 from the other border, 3 for the 3 elipses
-		// 	}
-		// 	return msg
-		// })
-		// return String(outputs.slice(window.min, window.max).join('\n'));
-		return `${cursor.row}, ${cursor.col}. height = ${window.height}`
+		// evaluate every line, and append it to the output
+		const outputs = text.map((expr) => {
+			if (!expr) {
+				return ""
+			}
+			let msg = ""
+			try {
+				msg = String(evaluate(expr, scope))
+			} catch (err) {
+				msg = String(err.message)
+			}
+			// if line too long, truncate it
+			msg = msg.replace("\n", " ")
+			if (msg.length > columns / 2) {
+				msg = msg.slice(0, Math.floor(columns / 2) - 7) + "..."
+				// magic number 7. comes from 2 characters from the border, 2 from the other border, 3 for the 3 elipses
+			}
+			return msg
+		})
+		return String(outputs.slice(window.min, window.max).join('\n'));
+		// return `${cursor.row}, ${cursor.col}. height = ${window.height}`
 	};
 
 	return (
