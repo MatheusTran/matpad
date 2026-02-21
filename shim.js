139b65c7c2221959fb1c4ad246f14aeb181ea324
MatheusTran <matheustran@gmail.com>
2026-02-21 14:20:41 +0100

    updated build scripts

--- shim.js
+++ shim.js
@@ -1,8 +1,9 @@
 // stole this file from: https://github.com/oven-sh/bun/issues/13405#issuecomment-2613782984
 // essentially to fix the yoga.wasm file bug when compiling
-const bin = Bun.file('bin.js');
+const file = './bin/bin.js'
+const bin = Bun.file(file);
 let content = await new Response(bin).text();
 const pattern = /var Yoga = await initYoga\(await E\(_\(import\.meta\.url\)\.resolve\("\.\/yoga\.wasm"\)\)\);/g;
 const replacement = `import initYogaAsm from 'yoga-wasm-web/asm'; const Yoga = initYogaAsm();`;
 content = content.replace(pattern, replacement);
-await Bun.write('bin.js', content);
+await Bun.write(file, content);
